e390f980cff084bf3a70410d5b06952cb9e83c5c
tirthasourav <dey.sourav777@gmail.com>
2025-02-26 17:41:42 -0500

    Configure GitHub Pages deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const isGithubPages = process.env.NODE_ENV === "production";
+
+const nextConfig = {
+  reactStrictMode: true,
+  output: "export", // Export as static HTML
+  basePath: isGithubPages ? "/your-repo-name" : "",
+  assetPrefix: isGithubPages ? "/your-repo-name/" : "",
+};
 
 export default nextConfig;
